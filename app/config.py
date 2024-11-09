7ef1a950bd5924b059b6bdd24904e17993cce4aa
unagami <kantaunagami@gmail.com>
2024-11-09 19:10:10 +0900

    PostgreSQLが使えるようにリンクの形を変更する処理を追記

--- app/config.py
+++ app/config.py
@@ -5,7 +5,14 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    
+    # 'postgres://' を 'postgresql://' に置き換える
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
