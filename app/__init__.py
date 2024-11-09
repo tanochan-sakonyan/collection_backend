c6e0d10c059053545aa692bbc82ade7156713a85
unagami <kantaunagami@gmail.com>
2024-11-09 20:18:28 +0900

    app/__init__.pyにmembersモデルの情報を追加

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ def create_app():
     # モデルをインポートしてマッピングを登録
     from app.users import models as user_models
     from app.events import models as event_models
+    from app.members import models as member_models
 
     # Register Blueprints
     from .users import users_bp
