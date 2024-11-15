5aa2c4f8a318205f46143ab8588d7d35d882537a
unagami <kantaunagami@gmail.com>
2024-11-15 19:45:37 +0900

    :sparkles: #10 developed change status feature

--- app/members/services.py
+++ app/members/services.py
@@ -1,6 +1,6 @@
 from app.users.models import User
 from app import db
-from .models import Member
+from .models import Member, Status
 
 def create_member_service(member_name: str, event_id: int) -> Member:
     member = Member(member_name, event_id)
@@ -22,5 +22,15 @@ def delete_member_service(member_id: int) -> None:
 def edit_member_name_service(member_id: int, member_name: str) -> Member:
     member = Member.query.get(member_id)
     member.member_name = member_name
+    db.session.commit()
+    return member
+
+def change_member_status_service(member_id: int, status: int) -> Member:
+    #状態を表す数は0から2のみ
+    if status not in [0, 1, 2]:
+        return None
+    
+    member = Member.query.get(member_id)
+    member.status = Status(status)
     db.session.commit()
     return member
