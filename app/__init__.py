c78e6c34d2b2e2bf425ccb3dc917e1bbc145b1dd
unagami <kantaunagami@gmail.com>
2024-11-24 19:34:04 +0900

    :+1: #35 botがLINEグループに追加された時、そのグループのメンバーのプロフィールを取得する

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 from flask_migrate import Migrate
+from linebot import LineBotApi, WebhookHandler
 
 db = SQLAlchemy()
 line_bot_api = None
+handler = None
 
 def create_app():
     app = Flask(__name__)
@@ -13,7 +15,11 @@ def create_app():
     app.config.from_object(Config)
 
     global line_bot_api
-    line_bot_api = app.config['LINE_CHANNEL_ACCESS_TOKEN']
+    line_bot_api = LineBotApi(app.config['LINE_CHANNEL_ACCESS_TOKEN'])  # global handler
+    global handler
+    handler = WebhookHandler(app.config['LINE_CHANNEL_SECRET'])  # global handler
+    
+
 
     # モデルをインポートしてマッピングを登録
     from app.users import models as user_models
