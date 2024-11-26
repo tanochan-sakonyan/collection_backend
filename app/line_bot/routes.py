0e1d0a3fba493c67d0fb9ee11bcf4c39ae867255
unagami <kantaunagami@gmail.com>
2024-11-26 17:38:40 +0900

    :construction: #38 ログ追加

--- app/line_bot/routes.py
+++ app/line_bot/routes.py
@@ -2,12 +2,14 @@ from flask import Blueprint, request, abort
 from app import handler
 from linebot.exceptions import InvalidSignatureError
 from . import line_bot_bp
+import logging
 
 @line_bot_bp.route('/line/webhook', methods=['POST'])
 def webhook():
     # LINEから送られてくる署名を検証
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
+    logging.info(f'Request body: {body}')
 
     try:
         #署名を検証、成功したらhandleに処理を移譲
