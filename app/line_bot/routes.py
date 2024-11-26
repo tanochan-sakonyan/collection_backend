2010dc94f025845c40b38ab6b6eb4132e66837ce
unagami <kantaunagami@gmail.com>
2024-11-26 17:43:53 +0900

    :construction: #38 ログ機能の追加

--- app/line_bot/routes.py
+++ app/line_bot/routes.py
@@ -4,12 +4,19 @@ from linebot.exceptions import InvalidSignatureError
 from . import line_bot_bp
 import logging
 
+# ログ設定を明示的に行う
+logging.basicConfig(level=logging.DEBUG)
+
 @line_bot_bp.route('/line/webhook', methods=['POST'])
 def webhook():
     # LINEから送られてくる署名を検証
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    logging.info(f'Request body: {body}')
+
+    # リクエスト情報をログ出力
+    logging.info("Received a request to /line/webhook")
+    logging.debug(f"Request body: {body}")
+    logging.debug(f"X-Line-Signature: {signature}")
 
     try:
         #署名を検証、成功したらhandleに処理を移譲
