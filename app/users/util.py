54826d8586c18b4f3adcb6267d0979cccb43d6e8
unagami <kantaunagami@gmail.com>
2024-11-28 19:46:19 +0900

    :bug: #46 get_user_line_id関数の修正

--- app/users/util.py
+++ app/users/util.py
@@ -1,13 +1,12 @@
 import requests
 
 def get_line_user_id(line_token):
-    url = 'https://api.line.me/oauth2/v2.1/userinfo'
+    url = 'https://api.line.me/v2/profile'
 
     headers = {
         'Authorization': f'Bearer {line_token}'
     }
 
     response = requests.get(url, headers=headers)
-    print(response.json())
 
-    return response.json()['sub']
+    return response.json()['userId']
